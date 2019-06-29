4deac357d1a58b7e516541d0dfe3244c66137e4c
Simone <simone.denada@gmail.com>
2019-06-29 14:14:02 +0200

    test i2c keyboard

--- keyboards/kbdfans/kbd67/rev2/rev2.c
+++ keyboards/kbdfans/kbd67/rev2/rev2.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "rev2.h"
+// #include "Adafruit_Trellis/Adafruit_Trellis.h"
 
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
